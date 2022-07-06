a0678cdd5040cfb5feaf9dd9f2d48738ebefe84c
kachisennin <107007259+kachisennin@users.noreply.github.com>
2022-07-06 11:45:38 +0100

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,11 +5,12 @@
  * followed by a new line
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	long int i, j, k, next;
+
 	j = 1;
+
 	k = 2;
 
 	for (i = 1; i <= 50; ++i)
@@ -17,8 +18,7 @@ int main(void)
 		if (j != 20365011074)
 		{
 			printf("%ld, ", j);
-		}
-		else
+		} else
 		{
 			printf("%ld\n", j);
 		}
