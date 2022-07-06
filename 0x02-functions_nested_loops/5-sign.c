83133d881d413eb77ce0a834ad48159662f146f3
kachisennin <107007259+kachisennin@users.noreply.github.com>
2022-07-06 11:05:37 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_sign -> prints sign based on condition
- * @n: argument passed
- * Return: 1, 0 -1 depending on condition
- */
+* print_sign - prints sign based on condition
+* @n: argument passed
+* Return: 1, 0 -1
+*/
 int print_sign(int n)
 {
 	if (n > 0)
@@ -17,7 +17,7 @@ int print_sign(int n)
 		_putchar('0');
 		return (0);
 	}
-	else if (n < 0)
+	else
 	{
 		_putchar('-');
 		return (-1);
