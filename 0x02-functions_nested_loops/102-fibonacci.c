3e5a1233cac729dacbd1775f79eb98db66cd0951
Onyekachi <igboobiakor.onyekachi@gmail.com>
2022-07-07 15:06:31 -0700

    Updated

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,7 +9,9 @@
 int main(void)
 {
 	long int i, j, k, next;
+
 	j = 1;
+
 	k = 2;
 
 	for (i = 1; i <= 50; ++i)
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,11 +3,13 @@
 /**
  * print_last_digit - prints the last digit of a number
  * @n: the int to extract the last digit from
- * Return: value of the last digit
+ * Return: value of the last digit 
  */
+
 int print_last_digit(int n)
 {
 	int a;
+
 	if (n < 0)
 	{
 		n = -n;
