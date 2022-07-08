0a6d5e76560c5f33536204511e5ddfc63c28feae
Onyekachi <igboobiakor.onyekachi@gmail.com>
2022-07-07 18:34:03 -0700

    hopefully the last commit

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -15,7 +15,9 @@ int main(void)
 	b = -98;
 	c = 0;
 
-	largest = largest_number\n", largest);
+	largest = largest_number(a, b, c);
+
+	printf("%d is the largest number\n", largest);
 
 	return (0);
 }
