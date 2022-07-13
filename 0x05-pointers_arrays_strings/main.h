609bc082b1d1bb0c93c82f0ed4e77ea0467d726b
Onyekachi <igboobiakor.onyekachi@gmail.com>
2022-07-12 17:18:12 -0700

    Tasks 3-6

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 int _atoi(char *s); 
+size_t _strlen(const char *str);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 void _puts(char *str);
