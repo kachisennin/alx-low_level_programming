8dba353c89779af9d2d231a2f8a6a2bb977906df
Onyekachi <igboobiakor.onyekachi@gmail.com>
2022-07-12 18:17:05 -0700

    edited

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,10 +4,10 @@
 
 int _putchar(char c);
 char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 int _atoi(char *s);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
